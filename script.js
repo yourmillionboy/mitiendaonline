8e35c2ed7fb1e8a8f97a5467612ad2003dfb7a07
yourmillionboy <151046927+yourmillionboy@users.noreply.github.com>
2023-11-17 17:26:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let carrito = [];
+
+function agregarAlCarrito(idProducto) {
+    // Lógica para agregar productos al carrito
+}
+
+function actualizarCarrito() {
+    // Lógica para actualizar la lista de productos en el carrito
+}
+
+function calcularTotal() {
+    // Lógica para calcular el total de la compra
+}
+
+function realizarCompra() {
+    // Lógica para procesar la compra (puede ser una redirección a un sistema de pago)
+}
+
+// Lógica para cargar dinámicamente los productos en la página
