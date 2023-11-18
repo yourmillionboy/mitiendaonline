ac85580b59efad32ec3053d340842517adeb369a
yourmillionboy <151046927+yourmillionboy@users.noreply.github.com>
2023-11-18 14:44:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,49 +5,13 @@ let productos = [
     // Agrega más productos según sea necesario
 ];
 
-// Estado de inicio de sesión
-let usuarioAutenticado = false;
-
-// Función para mostrar el formulario de inicio de sesión
-function mostrarFormularioLogin() {
-    const formularioLogin = document.getElementById('formulario-login');
-    formularioLogin.classList.remove('oculto');
-}
-
-// Función para ocultar el formulario de inicio de sesión
-function ocultarFormularioLogin() {
-    const formularioLogin = document.getElementById('formulario-login');
-    formularioLogin.classList.add('oculto');
-}
-
-// Función para cerrar el formulario de inicio de sesión
-function cerrarFormularioLogin() {
-    ocultarFormularioLogin();
-}
-
-// Función para iniciar sesión (simulada)
-function iniciarSesion() {
-    const usuarioInput = document.getElementById('usuario');
-    const contrasenaInput = document.getElementById('contrasena');
-
-    // Verifica si el usuario y la contraseña son válidos (simulados)
-    if (usuarioInput.value === 'usuario' && contrasenaInput.value === 'contrasena') {
-        usuarioAutenticado = true;
-        ocultarFormularioLogin();
-        alert('Inicio de sesión exitoso. ¡Bienvenido!');
-    } else {
-        alert('Inicio de sesión fallido. Usuario o contraseña incorrectos.');
-    }
-
-    // Limpia los campos del formulario
-    usuarioInput.value = '';
-    contrasenaInput.value = '';
-}
+// Estado del carrito de compras
+let carrito = [];
 
 // Función para mostrar todos los productos en la interfaz
 function mostrarProductos() {
     const contenedorProductos = document.getElementById('productos');
-    contenedorProductos.innerHTML = ''; // Limpiar el contenido existente
+    contenedorProductos.innerHTML = '';
 
     productos.forEach(producto => {
         const divProducto = document.createElement('div');
@@ -58,81 +22,11 @@ function mostrarProductos() {
             <p class="precio">$${producto.precio.toFixed(2)}</p>
             <button class="boton-accion" onclick="mostrarDetallesProducto(${producto.id})">Ver Detalles</button>
             <button class="boton-accion" onclick="agregarAlCarrito(${producto.id}, '${producto.nombre}', ${producto.precio})">Agregar al Carrito</button>
-            <button class="boton-accion" onclick="editarProducto(${producto.id})">Editar</button>
-            <button class="boton-accion" onclick="eliminarProducto(${producto.id})">Eliminar</button>
         `;
 
         contenedorProductos.appendChild(divProducto);
     });
 }
-
-// Función para agregar un nuevo producto
-function agregarProducto() {
-    const nombre = prompt('Ingrese el nombre del nuevo producto:');
-    const precio = parseFloat(prompt('Ingrese el precio del nuevo producto:'));
-    const descripcion = prompt('Ingrese la descripción del nuevo producto:');
-    const imagen = prompt('Ingrese el nombre de la imagen del nuevo producto (ejemplo: producto3.jpg):');
-
-    const nuevoProducto = {
-        id: productos.length + 1,
-        nombre: nombre,
-        precio: precio,
-        descripcion: descripcion,
-        imagen: imagen
-    };
-
-    productos.push(nuevoProducto);
-    mostrarProductos();
-}
-
-// Función para editar un producto existente
-function editarProducto(idProducto) {
-    const productoEditar = productos.find(producto => producto.id === idProducto);
-
-    const nuevoNombre = prompt('Ingrese el nuevo nombre:', productoEditar.nombre);
-    const nuevoPrecio = parseFloat(prompt('Ingrese el nuevo precio:', productoEditar.precio));
-    const nuevaDescripcion = prompt('Ingrese la nueva descripción:', productoEditar.descripcion);
-    const nuevaImagen = prompt('Ingrese el nuevo nombre de la imagen (ejemplo: producto3.jpg):', productoEditar.imagen);
-
-    productoEditar.nombre = nuevoNombre;
-    productoEditar.precio = nuevoPrecio;
-    productoEditar.descripcion = nuevaDescripcion;
-    productoEditar.imagen = nuevaImagen;
-
-    mostrarProductos();
-}
-
-// Función para eliminar un producto
-function eliminarProducto(idProducto) {
-    productos = productos.filter(producto => producto.id !== idProducto);
-    mostrarProductos();
-}
-
-// Función para mostrar detalles de un producto
-function mostrarDetallesProducto(idProducto) {
-    // Aquí puedes agregar lógica para cargar los detalles del producto desde una fuente de datos
-    const nombre = `Producto ${idProducto}`;
-    const descripcion = `Descripción detallada del ${nombre}.`;
-    const precio = Math.random() * 100 + 50;
-
-    // Mostrar detalles en la interfaz
-    const imagenDetalle = document.getElementById('imagen-detalle');
-    const nombreDetalle = document.getElementById('nombre-detalle');
-    const descripcionDetalle = document.getElementById('descripcion-detalle');
-    const precioDetalle = document.getElementById('precio-detalle');
-
-    imagenDetalle.src = `imagen_${idProducto}.jpg`; // Ajusta la ruta según tus necesidades
-    nombreDetalle.textContent = nombre;
-    descripcionDetalle.textContent = descripcion;
-    precioDetalle.textContent = `Precio: $${precio.toFixed(2)}`;
-
-    const detallesProducto = document.getElementById('detalles-producto');
-    detallesProducto.classList.remove('oculto');
-}
-// ... (Código anterior)
-
-// Estado del carrito de compras
-let carrito = [];
 
 // Función para agregar un producto al carrito
 function agregarAlCarrito(idProducto, nombre, precio) {
@@ -154,8 +48,7 @@ function actualizarCarrito() {
     const listaCarrito = document.getElementById('lista-carrito');
     const totalCarrito = document.getElementById('total');
 
-    listaCarrito.innerHTML = ''; // Limpiar el contenido existente
-
+    listaCarrito.innerHTML = '';
     let total = 0;
 
     carrito.forEach(item => {
@@ -171,49 +64,36 @@ function actualizarCarrito() {
     carritoElemento.classList.remove('oculto');
 }
 
-// Función para realizar una compra
-function realizarCompra() {
-    if (carrito.length === 0) {
-        alert('¡El carrito está vacío! Agrega productos antes de realizar una compra.');
-        return;
+// Función para mostrar detalles de un producto
+function mostrarDetallesProducto(idProducto) {
+    // Esta función puede expandirse para cargar detalles del producto desde una fuente de datos
+    const productoSeleccionado = productos.find(producto => producto.id === idProducto);
+
+    if (productoSeleccionado) {
+        const imagenDetalle = document.getElementById('imagen-detalle');
+        const nombreDetalle = document.getElementById('nombre-detalle');
+        const descripcionDetalle = document.getElementById('descripcion-detalle');
+        const precioDetalle = document.getElementById('precio-detalle');
+
+        imagenDetalle.src = productoSeleccionado.imagen;
+        nombreDetalle.textContent = productoSeleccionado.nombre;
+        descripcionDetalle.textContent = productoSeleccionado.descripcion;
+        precioDetalle.textContent = `Precio: $${productoSeleccionado.precio.toFixed(2)}`;
+
+        const detallesProducto = document.getElementById('detalles-producto');
+        detallesProducto.classList.remove('oculto');
+    } else {
+        alert('No se encontraron detalles para el producto seleccionado.');
     }
-
-    const confirmacion = confirm('¿Deseas realizar la compra?');
-    if (confirmacion) {
-        alert('¡Compra realizada con éxito! Gracias por tu compra.');
-        vaciarCarrito();
-    }
 }
 
-// Función para vaciar el carrito
-function vaciarCarrito() {
-    carrito = [];
-    actualizarCarrito();
+// Función para cerrar los detalles del producto
+function cerrarDetallesProducto() {
+    const detallesProducto = document.getElementById('detalles-producto');
+    detallesProducto.classList.add('oculto');
 }
 
-// Función para mostrar productos destacados
-function mostrarProductosDestacados() {
-    const contenedorProductosDestacados = document.getElementById('productos-destacados-lista');
-    contenedorProductosDestacados.innerHTML = '';
+// ... (Otras funciones según sea necesario)
 
-    // Puedes agregar lógica para cargar productos destacados desde una fuente de datos
-    const productosDestacados = [
-        { nombre: "Reloj de Edición Especial", precio: 899.99, imagen: "reloj.jpg" },
-        { nombre: "Chaqueta de Cuero Premium", precio: 799.99, imagen: "chaqueta.jpg" },
-    ];
-
-    productosDestacados.forEach(producto => {
-        const divProductoDestacado = document.createElement('div');
-        divProductoDestacado.className = 'producto-destacado';
-        divProductoDestacado.innerHTML = `
-            <img src="${producto.imagen}" alt="${producto.nombre}">
-            <h2>${producto.nombre}</h2>
-            <p class="precio">$${producto.precio.toFixed(2)}</p>
-            <button class="boton-accion" onclick="agregarAlCarrito(${carrito.length + 1}, '${producto.nombre}', ${producto.precio})">Agregar al Carrito</button>
-        `;
-
-        contenedorProductosDestacados.appendChild(divProductoDestacado);
-    });
-}
-
-// ... (Funciones anteriores)
+// Inicializar la página mostrando los productos
+mostrarProductos();
